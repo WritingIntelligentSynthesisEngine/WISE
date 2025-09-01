c0656237231642385d34db883ff8185e88de59f6
骑士姬 <2067834160@qq.com>
2025-09-01 22:42:33 +0800

    perf: 提供接口文档样例

--- backend/core/routers.py
+++ backend/core/routers.py
@@ -3,9 +3,13 @@ from typing import Any
 
 from ninja import Router
 
-router: Router = Router()
+router: Router = Router(tags=["测试"])
 
 
-@router.get("/hello")
+@router.get(
+    "/hello",
+    summary="连通测试",
+)
 def hello(request: Any) -> str:
+    """用于连通测试, 返回一个字符串"""
     return "Hello World!"
