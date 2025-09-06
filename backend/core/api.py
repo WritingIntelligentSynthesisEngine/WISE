f581f61bd517b3ee8fa8ac55b5c9b83eb1578ffa
骑士姬 <2067834160@qq.com>
2025-09-07 01:18:27 +0800

    refactor: 自定义 token 接口

--- backend/core/api.py
+++ backend/core/api.py
@@ -1,16 +1,16 @@
 # core/api.py
 from ninja_extra import NinjaExtraAPI
-from ninja_jwt.controller import NinjaJWTDefaultController
 
 from core.routers import router as core_router
 from book.routers import router as book_router
+from core.routers import NinjaJWTDefaultController
 
 
 api: NinjaExtraAPI = NinjaExtraAPI(title="AllBookCloud API")
 
-# JWT API
-api.register_controllers(NinjaJWTDefaultController)
 # 服务核心 API
 api.add_router("/core", core_router)
+# JWT API
+api.register_controllers(NinjaJWTDefaultController)
 # 书籍与文章 API
 api.add_router("/book", book_router)
